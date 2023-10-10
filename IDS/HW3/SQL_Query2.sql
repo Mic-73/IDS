38bbe76b44cc3db251350c89f04c141f1db7cf67
Mike W <143733799+Mic-73@users.noreply.github.com>
2023-10-10 15:15:05 -0400

    Delete IDS directory

--- IDS/HW3/SQL_Query2.sql
+++
@@ -1,7 +0,0 @@
--- ----------------------------------------------
---  Query 2: 
---  Display total count of countries after data cleaning.
--- ----------------------------------------------
-
-select count(Country)
-from lifeExpectancy;
